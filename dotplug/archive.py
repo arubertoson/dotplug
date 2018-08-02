c7a96c9fb5c1486087bea398b017de2f7ef3f2e0
Marcus Albertsson <marcus.arubertoson@gmail.com>
2018-08-02 21:55:29 +0100

    layout

--- dotplug/archive.py
+++ dotplug/archive.py
@@ -2,15 +2,108 @@
 This module contains functionality relating to archives
 """
 import os
+import abc
 import tarfile
 import zipfile
+import asyncio
 
 import aiohttp
 import aiofiles
 
 
+async def download(session, task):
+    """
+    Download repo given from the task url
+    """
+    archive, bar = task.archive, task.bar
+    async with session.get(task.url) as response:
+        size = int(response.headers.get('content-length', 0))
+
+        # Download archive if does not exist
+        dirname = os.path.dirname(archive)
+        if not os.path.exists(dirname):
+            os.makedirs(os.path.dirname(archive))
+
+        async with aiofiles.open(archive, mode='wb') as f:
+            total = 0
+            async for chunk in response.content.iter_chunked(1024):
+                await f.write(chunk)
+                total += len(chunk)
+
+                bar.message.write(f'{total}/{size} ... Downloading')
+            bar.message.clear()
+
+
+async def ensure_archive(task):
+    """
+    Ensure that the taks archive exists and we have something to act upon.
+    """
+    archive, bar = task.archive, task.bar
+
+    # Download archive if it doesn't exist
+    if os.path.exists(archive):
+        if not task.type == 'appimage':
+            bar.message.write('Validating Archive ... ')
+            validator = {
+                'tar': validate_tar,
+                'zip': validate_zip,
+            }[task.type]
+            valid = await bar.loader.wait_for(validator, archive)
+        valid = True
+    else:
+        valid = False
+
+    if not valid:
+        async with aiohttp.ClientSession() as session:
+            await download(session, task)
+
+
+class AIOCompressedFile(abc.ABC):
+    """
+    Base Object for Compressed File Archives
+    """
+    magic = None
+    file_type = None
+    mime_type = None
+    proper_extensio = None
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.accessor = self.open
+
+    @classmethod
+    def is_magic(cls, data):
+        return data.startswith(cls.magic)
+
+    async def __aenter__(self):
+        await self.open()
+
+    async def __aexit__(self, *args):
+        return asyncio.create_task(self.close())
+
+    @abc.abstractmethod
+    def open(self):
+        return None
+
+    @abc.abstractmethod
+    def close(self):
+        return None
+
+    @abc.abstractmethod
+    def validate(self):
+        return None
+
+    @abc.abstractmethod
+    def unpack(self, location):
+        return None
+
+
 class ZipFile(zipfile.ZipFile):
     """
+    Override on the extract method in ZipFile
+
+    Currently ZipFile does not retain the proper permissions, this override
+    deals with that issue
     """
 
     def extract(self, member, path=None, pwd=None):
@@ -26,6 +119,30 @@ class ZipFile(zipfile.ZipFile):
         # workaround that is currently needed
         os.chmod(ret_val, 0o755)
         return ret_val
+
+
+class ZipArchive(ArchiveFile):
+    magic = '\x50\x4b\x03\x04'
+    file_type = 'zip'
+
+    def open(self):
+        return ZipFile(self.filename)
+
+    def unpack(self, location):
+        """
+        Unpacks the zipfile to provided dest location
+
+        If the archive contains a top directory, it will be ignored
+        """
+        relpath = os.path.relpath
+
+        with ZipFile(archive) as z:
+            prefix = os.path.commonprefix(z.namelist())
+            for m in z.filelist:
+                if m.is_dir() or m.filename in ('.', '/'):
+                    continue
+                m.filename = relpath(m.filename, prefix)
+                z.extract(m, path=dest)
 
 
 def unzip(archive, dest):
@@ -87,50 +204,3 @@ def validate_tar(archive):
     else:
         res = True
     return res
-
-
-async def download(session, task):
-    """
-    Download repo given from the task url
-    """
-    archive, bar = task.archive, task.bar
-    async with session.get(task.url) as response:
-        size = int(response.headers.get('content-length', 0))
-
-        # Download archive if does not exist
-        dirname = os.path.dirname(archive)
-        if not os.path.exists(dirname):
-            os.makedirs(os.path.dirname(archive))
-
-        async with aiofiles.open(archive, mode='wb') as f:
-            total = 0
-            async for chunk in response.content.iter_chunked(1024):
-                await f.write(chunk)
-                total += len(chunk)
-
-                bar.message.write(f'{total}/{size} ... Downloading')
-            bar.message.clear()
-
-
-async def ensure_archive(task):
-    """
-    Ensure that the taks archive exists and we have something to act upon.
-    """
-    archive, bar = task.archive, task.bar
-
-    # Download archive if it doesn't exist
-    if os.path.exists(archive):
-        if not task.type == 'appimage':
-            bar.message.write('Validating Archive ... ')
-            validator = {
-                'tar': validate_tar,
-                'zip': validate_zip,
-            }[task.type]
-            valid = await bar.loader.wait_for(validator, archive)
-        valid = True
-    else:
-        valid = False
-
-    if not valid:
-        async with aiohttp.ClientSession() as session:
-            await download(session, task)
